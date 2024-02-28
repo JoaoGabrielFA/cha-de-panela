9c42ab48b342c7fc636b8fafda141eb04915227d
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2024-02-28 13:18:28 -0300

    updating

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
+import styles from './Home.module.css';
+import { FaHeart } from "react-icons/fa";
 
 function Home() {
   return (
     <>
-      <Header/>
-      
-      <Footer/>
+      <Header />
+      <main className={styles.main}>
+        <section className={styles.banner}>
+          <div className={`${styles.image} ${styles.dany}`}></div>
+          <div className={`${styles.image} ${styles.riquelme}`}></div>
+          <span className={styles.text}>Bem vindos ao nosso&nbsp;
+            <span className={styles.textPink}>Chá de Panela</span>
+          </span>
+          <FaHeart size={64}/>
+        </section>
+      </main>
+      <Footer />
     </>
   )
 }
