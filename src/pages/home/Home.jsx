dcd6ec7d3c7fb2a619c625017367558bcdc77dc3
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2024-02-28 14:16:01 -0300

    updating

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,15 +10,17 @@ function Home() {
       <Header />
       <main className={styles.main}>
         <section className={styles.banner}>
-          <div className={`${styles.image} ${styles.dany}`}></div>
-          <div className={`${styles.image} ${styles.riquelme}`}></div>
           <span className={styles.text}>Bem vindos ao nosso&nbsp;
             <span className={styles.textPink}>Chá de Panela</span>
           </span>
-          <FaHeart size={64}/>
+          <div className={`${styles.image} ${styles.dany}`}></div>
+          <div className={`${styles.image} ${styles.riquelme}`}></div>
+          <FaHeart size={64} />
         </section>
+        <span className={styles.textMessage}>Contamos com a sua presença, no dia 20 de abril, na rua Honório Lemos Nº56</span>
+        <a className={styles.seeMore}>Ver Produtos</a>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
