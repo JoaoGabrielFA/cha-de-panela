964697e04ca3dd18af598cb6b85bd2991cfdba54
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2024-03-05 19:35:57 -0300

    updating

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,22 @@ import SandwichMenu from '../sandwich/SandwichMenu';
 import { IoBed } from "react-icons/io5";
 import { FaToilet, FaCouch } from "react-icons/fa";
 import { ImSpoonKnife } from "react-icons/im";
+import { Link } from 'react-router-dom';
 
 function Header() {
+  const [linksCozinha, setLinksCozinha] = useState(false);
+  const [linksSala, setLinksSala] = useState(false);
+
+  const toggleCozinha = () => {
+    setLinksCozinha(!linksCozinha);
+    setLinksSala(false);
+  };
+
+  const toggleSala = () => {
+    setLinksCozinha(false);
+    setLinksSala(!linksSala);
+  };
+
   const [active, setActive] = useState(false);
 
   const handlerButton = () => {
@@ -17,7 +31,32 @@ function Header() {
       <div className={`${styles.sandwich} ${active ? styles.active : ''}`}>
         <div className={`${styles.sandwichButton} ${active ? styles.active : ''}`} onClick={handlerButton}></div>
       </div>
-      <div className={`${styles.categories} ${active ? styles.active : ''}`}>
+      <div className={`${styles.categorias} ${active ? styles.active : ''}`}>
+        <div className={styles.categoria} onClick={toggleCozinha}>
+          COZINHA
+          {linksCozinha && (
+            <>
+              <Link to="/produtos">Ver Todos</Link>
+              <Link to="/produtos">Talheres</Link>
+              <Link to="/produtos">Potes</Link>
+              <Link to="/produtos">Panelas</Link>
+            </>
+          )}
+        </div>
+
+        <div className={styles.categoria} onClick={toggleSala}>
+          SALA
+          {linksSala && (
+            <>
+              <Link to="/produtos">Ver Todos</Link>
+              <Link to="/produtos">Tapetes</Link>
+              <Link to="/produtos">Almofadas</Link>
+              <Link to="/produtos">Enfeites</Link>
+            </>
+          )}
+        </div>
+      </div>
+      {/* <div className={`${styles.categories} ${active ? styles.active : ''}`}>
         <div className={styles.category}>
           <IoBed />
           <span>Quarto</span>
@@ -34,7 +73,7 @@ function Header() {
           <ImSpoonKnife />
           <span>Cozinha</span>
         </div>
-      </div>
+      </div> */}
     </header>
   )
 }
--- src/pages/produtos/Produtos.jsx
+++ src/pages/produtos/Produtos.jsx
@@ -10,8 +10,13 @@ function Produtos() {
 
         <span className={styles.produtos_titulo}>TODOS OS PRODUTOS</span>
 
-        <div className={styles.produto}>
-          <div className={styles.produto_img} style={{ backgroundImage: `url('https://a-static.mlcdn.com.br/800x560/kit-2-cadeiras-reforcadas-estofadas-mesa-cozinha-luxo-linho-balaqui-decor/balaquiestofadosltda/15883002572/0c8eed182060d51829a03c5bb1b0e7b8.jpeg')` }}></div>
+        <div className={styles.produtoT}>
+          <div className={styles.produtoT_img} style={{ backgroundImage: `url('https://m.media-amazon.com/images/I/41G3itFxggL._AC_SX425_.jpg')`}}></div>
+          <span className={styles.produtoT_nome}>FACA</span>
+        </div>
+
+        {/* <div className={styles.produto}>
+          <div className={styles.produto_img} https://a-static.mlcdn.com.br/800x560/kit-2-cadeiras-reforcadas-estofadas-mesa-cozinha-luxo-linho-balaqui-decor/balaquiestofadosltda/15883002572/0c8eed182060d51829a03c5bb1b0e7b8.jpeg')` }}></div>
           <div className={styles.produto_dados}>
             <span className={styles.produto_titulo}>KIT 2 Cadeiras</span>
             <span className={styles.produto_loja}>por Magazine Luiza</span>
@@ -30,7 +35,7 @@ function Produtos() {
             <span className={styles.produto_parcelado}>em até 4x R$ 52,49 sem juros</span>
             <Link className={styles.produto_link} target='_blank' to={'https://www.amazon.com.br/Torneira-Cozinha-Docol-00801306-Cromado/dp/B0792ND5FD/ref=asc_df_B0792ND5FD/?tag=googleshopp00-20&linkCode=df0&hvadid=379707889890&hvpos=&hvnetw=g&hvrand=5456916210847802054&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1031419&hvtargid=pla-810604235693&psc=1&mcid=5073fe3ed684304f9477929582ea3b2c'}>Ver Produto</Link>
           </div>
-        </div>
+        </div> */}
       </main>
     </>
 
