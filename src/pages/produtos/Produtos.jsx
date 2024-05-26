2445cb81af9640c3d5805b6619c379d18948be59
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2024-05-26 16:47:55 -0300

    updating

--- src/pages/produtos/Produtos.jsx
+++ src/pages/produtos/Produtos.jsx
@@ -1,17 +1,12 @@
-import { Link, useParams } from 'react-router-dom';
-import Header from '../../components/header/Header';
 import styles from './Produtos.module.css';
-// import data from '../../data/api.json';
 import { IoBed } from "react-icons/io5";
-import { FaKitchenSet, FaBaby } from "react-icons/fa6";
-import { FaToilet, FaBabyCarriage } from "react-icons/fa";
+import { FaKitchenSet } from "react-icons/fa6";
+import { FaToilet } from "react-icons/fa";
 import { useEffect, useState } from 'react';
-import { GiBabyBottle } from "react-icons/gi";
 import { MdBabyChangingStation } from "react-icons/md";
 
 function Produtos() {
   const [categoria, setCategoria] = useState('quarto');
-  const [panela, setPanela] = useState(true);
   const [itens, setItens] = useState([]);
 
   useEffect(() => {
@@ -190,42 +185,23 @@ function Produtos() {
           </div>
         </div>
       </div>
-      <div className={styles.folder}>
-        <div className={styles.folderButtons}>
-          <div className={`${styles.folderButton} ${panela ? '' : styles.desactive}`} onClick={() => setPanela(true)}>Chá de Panela</div>
-          <div className={`${styles.folderButton} ${panela ? styles.desactive : ''}`} onClick={() => setPanela(false)}>Chá de Fralda</div>
+      <div className={styles.buttons}>
+        <div className={styles.button}>
+          <IoBed onClick={() => setCategoria('quarto')} />
+          <span>Quarto</span>
         </div>
-        {panela ?
-          <div className={styles.buttons}>
-            <div className={styles.button}>
-              <IoBed onClick={() => setCategoria('quarto')} />
-              <span>Quarto</span>
-            </div>
-            <div className={styles.button}>
-              <FaKitchenSet onClick={() => setCategoria('cozinha')} />
-              <span>Cozinha</span>
-            </div>
-            <div className={styles.button}>
-              <FaToilet onClick={() => setCategoria('banheiro')} />
-              <span>Banheiro</span>
-            </div>
-          </div>
-          :
-          <div className={styles.buttons}>
-            <div className={styles.button}>
-              <MdBabyChangingStation onClick={() => setCategoria('higiene')} />
-              <span>Higiene</span>
-            </div>
-            <div className={styles.button}>
-              <FaBaby onClick={() => setCategoria('vestuario')} />
-              <span>Vestuário</span>
-            </div>
-            <div className={styles.button}>
-              <GiBabyBottle onClick={() => setCategoria('acessorios')} />
-              <span>Acessórios</span>
-            </div>
-          </div>
-        }
+        <div className={styles.button}>
+          <FaKitchenSet onClick={() => setCategoria('cozinha')} />
+          <span>Cozinha</span>
+        </div>
+        <div className={styles.button}>
+          <FaToilet onClick={() => setCategoria('banheiro')} />
+          <span>Banheiro</span>
+        </div>
+        <div className={styles.button}>
+          <MdBabyChangingStation onClick={() => setCategoria('higiene')} />
+          <span>Higiene</span>
+        </div>
       </div>
     </main>
   )
